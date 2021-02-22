fee2c50cf9afa05f4e2089a54fe0c5fce1c876f7
tdw2062 <36544515+tdw2062@users.noreply.github.com>
2021-02-22 15:45:37 -0500

    Created loop with conditional

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ let pokemonList = [
     { name: 'Squirtle', height: 0.5, types: ['Water']}
   ];
 
+//This loops through the Pokemon objects and writes them to the DOM
+//The condition writes "Wow, that's big!" next to any over 0.6
+
+for (let i=0; i <pokemonList.length; i++){
+  document.write(pokemonList[i].name + "(height: " + pokemonList[i].height  + ")");
+  if (pokemonList[i].height>0.6) {
+    document.write("Wow, that's big!");
+  };
+  document.write("<br><br>");
+}
