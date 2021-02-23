e28e03448d5f0fee22479966955af5abe758f10e
tdw2062 <36544515+tdw2062@users.noreply.github.com>
2021-02-23 17:28:05 -0500

    An IIFE is implemented to protect local variables

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 //This program creates a pokemon object
 
-//This is an array of pokemon objects
-let pokemonList = [
+//This wraps the pokemon repository in an IIFE
+let pokemonRepository = (function() {
+  let pokemonList = [
     { name: 'Bulbasaur', height: 0.7, types: ['Grass', 'Poison']},
     { name: 'Charmander', height: 0.6, types: ['Fire']},
     { name: 'Squirtle', height: 0.5, types: ['Water']}
-  ];
+  ]; 
+
+  return {
+    add: function(pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function(){
+      return pokemonList;
+    }
+  };
+})();
 
 //This loop uses the forEach to loop through the Pokemon objects and write them to the DOM
-pokemonList.forEach(function(user){
+//First the function getAll from pokemonRepository is used to get the array
+pokemonRepository.getAll().forEach(function(user){
   document.write(user.name + " is " + user.height + " tall and has types " + user.types + ".");
   document.write("<br><br>");
 
